b0e59f7bbf95f84d5a282d4403d086018f148ff4
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2024-09-12 22:18:37 +0700

    deleted empty line

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -19,7 +19,6 @@ func (s *Server) RegisterRoutes() http.Handler {
 func (s *Server) HelloWorldHandler(c *gin.Context) {
 	resp := make(map[string]string)
 	resp["message"] = "Hello World"
-
 	c.JSON(http.StatusOK, resp)
 }
 

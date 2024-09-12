e3b848ce7dc08d542e4093ee9b586cc43ae953a7
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2024-09-12 22:20:01 +0700

    created interface for common database interactions

--- internal/database/database.go
+++ internal/database/database.go
@@ -11,10 +11,30 @@ import (
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/joho/godotenv/autoload"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 // Service represents a service that interacts with a database.
 type Service interface {
+	Model(value interface{}) *gorm.DB
+	Select(query interface{}, args ...interface{}) *gorm.DB
+	Find(out interface{}, where ...interface{}) *gorm.DB
+	Exec(sql string, values ...interface{}) *gorm.DB
+	First(out interface{}, where ...interface{}) *gorm.DB
+	Raw(sql string, values ...interface{}) *gorm.DB
+	Create(value interface{}) *gorm.DB
+	Save(value interface{}) *gorm.DB
+	Updates(value interface{}) *gorm.DB
+	Delete(value interface{}) *gorm.DB
+	Where(query interface{}, args ...interface{}) *gorm.DB
+	Preload(column string, conditions ...interface{}) *gorm.DB
+	Scopes(funcs ...func(*gorm.DB) *gorm.DB) *gorm.DB
+	ScanRows(rows *sql.Rows, result interface{}) error
+	DropTableIfExists(value interface{}) error
+	AutoMigrate(value interface{}) error
+	Transaction(fc func(tx Service) error) (err error)
+
 	// Health returns a map of health status information.
 	// The keys and values in the map are service-specific.
 	Health() map[string]string
@@ -25,7 +45,7 @@ type Service interface {
 }
 
 type service struct {
-	db *sql.DB
+	db *gorm.DB
 }
 
 var (
@@ -43,8 +63,10 @@ func New() Service {
 	if dbInstance != nil {
 		return dbInstance
 	}
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&search_path=%s", username, password, host, port, database, schema)
-	db, err := sql.Open("pgx", connStr)
+	// connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&search_path=%s", username, password, host, port, database, schema)
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, username, password, database, port)
+	// db, err := sql.Open("pgx", connStr)
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -52,6 +74,101 @@ func New() Service {
 		db: db,
 	}
 	return dbInstance
+}
+
+func (s *service) Model(value interface{}) *gorm.DB {
+	return s.db.Model(value)
+}
+
+func (s *service) Select(query interface{}, args ...interface{}) *gorm.DB {
+	return s.db.Select(query, args...)
+}
+
+func (s *service) Find(dest interface{}, conds ...interface{}) *gorm.DB {
+	return s.db.Find(dest, conds...)
+}
+
+func (s *service) Exec(sql string, values ...interface{}) *gorm.DB {
+	return s.db.Exec(sql, values...)
+}
+
+func (s *service) First(dest interface{}, conds ...interface{}) *gorm.DB {
+	return s.db.First(dest, conds...)
+}
+
+func (s *service) Raw(sql string, values ...interface{}) *gorm.DB {
+	return s.db.Raw(sql, values)
+}
+
+func (s *service) Create(value interface{}) *gorm.DB {
+	return s.db.Create(value)
+}
+
+func (s *service) Save(value interface{}) *gorm.DB {
+	return s.db.Save(value)
+}
+
+func (s *service) Updates(value interface{}) *gorm.DB {
+	return s.db.Updates(value)
+}
+
+func (s *service) Delete(value interface{}) *gorm.DB {
+	return s.db.Delete(value)
+}
+
+func (s *service) Where(query interface{}, args ...interface{}) *gorm.DB {
+	return s.db.Where(query, args...)
+}
+
+func (s *service) Preload(column string, conditions ...interface{}) *gorm.DB {
+	return s.db.Preload(column, conditions...)
+}
+
+func (s *service) Scopes(funcs ...func(*gorm.DB) *gorm.DB) *gorm.DB {
+	return s.db.Scopes(funcs...)
+}
+
+func (s *service) ScanRows(rows *sql.Rows, result interface{}) error {
+	return s.db.ScanRows(rows, result)
+}
+
+func (s *service) DropTableIfExists(value interface{}) error {
+	return s.db.Migrator().DropTable(value)
+}
+
+func (s *service) AutoMigrate(value interface{}) error {
+	return s.db.AutoMigrate(value)
+}
+
+func (s *service) Transaction(fc func(tx Service) error) (err error) {
+	panicked := true
+	tx := s.db.Begin()
+	defer func() {
+		if panicked || err != nil {
+			tx.Rollback()
+		}
+	}()
+
+	txservice := &service{}
+	txservice.db = tx
+	err = fc(txservice)
+
+	if err == nil {
+		err = tx.Commit().Error
+	}
+
+	panicked = false
+	return
+}
+
+// Close closes the database connection.
+// It logs a message indicating the disconnection from the specific database.
+// If the connection is successfully closed, it returns nil.
+// If an error occurs while closing the connection, it returns the error.
+func (s *service) Close() error {
+	log.Printf("Disconnected from database: %s", database)
+	sqlDb, _ := s.db.DB()
+	return sqlDb.Close()
 }
 
 // Health checks the health of the database connection by pinging the database.
@@ -62,8 +179,9 @@ func (s *service) Health() map[string]string {
 
 	stats := make(map[string]string)
 
+	sqlDb, _ := s.db.DB()
 	// Ping the database
-	err := s.db.PingContext(ctx)
+	err := sqlDb.PingContext(ctx)
 	if err != nil {
 		stats["status"] = "down"
 		stats["error"] = fmt.Sprintf("db down: %v", err)
@@ -76,7 +194,7 @@ func (s *service) Health() map[string]string {
 	stats["message"] = "It's healthy"
 
 	// Get database stats (like open connections, in use, idle, etc.)
-	dbStats := s.db.Stats()
+	dbStats := sqlDb.Stats()
 	stats["open_connections"] = strconv.Itoa(dbStats.OpenConnections)
 	stats["in_use"] = strconv.Itoa(dbStats.InUse)
 	stats["idle"] = strconv.Itoa(dbStats.Idle)
@@ -103,13 +221,4 @@ func (s *service) Health() map[string]string {
 	}
 
 	return stats
-}
-
-// Close closes the database connection.
-// It logs a message indicating the disconnection from the specific database.
-// If the connection is successfully closed, it returns nil.
-// If an error occurs while closing the connection, it returns the error.
-func (s *service) Close() error {
-	log.Printf("Disconnected from database: %s", database)
-	return s.db.Close()
 }

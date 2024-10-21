f4c55d47222dbc9407e6a37c364231a7d1ab099f
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2024-10-22 02:09:01 +0700

    created handler for account register

---
+++ internal/app/account/handler.go
@@ -0,0 +1,47 @@
+package account
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hadihalimm/cafebuzz-backend/internal/api/request"
+	"github.com/hadihalimm/cafebuzz-backend/internal/api/response"
+)
+
+type Handler struct {
+	service Service
+}
+
+func NewHandler(service Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) Create(c *gin.Context) {
+	var input request.RegisterRequest
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		response := response.Response{
+			Success: false,
+			Message: "Something went wrong",
+			Data:    err.Error(),
+		}
+		c.JSON(http.StatusBadRequest, response)
+	}
+
+	result, err := h.service.Create(input)
+	if err != nil {
+		response := response.Response{
+			Success: false,
+			Message: "Something went wrong",
+			Data:    err.Error(),
+		}
+		c.JSON(http.StatusBadRequest, response)
+	}
+
+	response := response.Response{
+		Success: true,
+		Message: "Successfully created new account!",
+		Data:    result,
+	}
+	c.JSON(http.StatusCreated, response)
+}

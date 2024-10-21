54ceab9509a2d8e7b4d2cfeb5455a51060fa419d
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2024-10-22 02:07:56 +0700

    created service for register

---
+++ internal/app/account/service.go
@@ -0,0 +1,43 @@
+package account
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/hadihalimm/cafebuzz-backend/internal/api/request"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Service interface {
+	Create(request request.RegisterRequest) (Account, error)
+}
+
+type service struct {
+	repo     Repository
+	validate *validator.Validate
+}
+
+func NewService(repository Repository, validate *validator.Validate) Service {
+	return &service{
+		repo:     repository,
+		validate: validate,
+	}
+}
+
+func (s *service) Create(request request.RegisterRequest) (Account, error) {
+	newAccount := Account{}
+	validateError := s.validate.Struct(request)
+	if validateError != nil {
+		return newAccount, validateError
+	}
+	newAccount.Username = request.Username
+	newAccount.FirstName = request.FirstName
+	newAccount.LastName = request.LastName
+	newAccount.Email = request.Email
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+	newAccount.PasswordHash = string(hashedPassword)
+
+	repoError := s.repo.Create(newAccount)
+	if repoError != nil {
+		return newAccount, repoError
+	}
+	return newAccount, nil
+}

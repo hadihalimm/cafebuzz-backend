5fd7d8344ca4e68fdfdaa03d0ee999e78b49cead
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2024-10-22 02:09:44 +0700

    added account register route

---
+++ internal/api/routes.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := gin.Default()
+
+	v1 := r.Group("api/v1")
+	{
+		v1.POST("/account", s.accountHandler.Create)
+	}
+
+	return r
+}

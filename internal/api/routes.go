5fd7e6f5a6639ac2a0fd3eb8f1733ca6f27bb3e5
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2024-10-22 21:29:43 +0700

    inserted auth middleware

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -11,7 +11,13 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 	v1 := r.Group("api/v1")
 	{
-		v1.POST("/account", s.accountHandler.Register)
+		v1.POST("/register", s.accountHandler.Register)
+		v1.POST("/login", s.accountHandler.Login)
+
+		account := v1.Group("/account")
+		{
+			account.GET("/", s.RequireAuth, s.accountHandler.GetCurrentAccount)
+		}
 	}
 
 	return r

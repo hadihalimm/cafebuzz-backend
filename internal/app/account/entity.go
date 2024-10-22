e2d2034fce999fcf74aeb6082d2a03ffaf9cb4bb
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2024-10-22 15:57:06 +0700

    added json tags and uuid

--- internal/app/account/entity.go
+++ internal/app/account/entity.go
@@ -1,16 +1,20 @@
 package account
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Account struct {
-	ID             uint   `gorm:"primaryKey"`
-	Username       string `gorm:"unique;not null"`
-	FirstName      string `gorm:"not null"`
-	LastName       string `gorm:"not null"`
-	Email          string `gorm:"unique;not null"`
-	PasswordHash   string `gorm:"not null"`
-	ProfilePicture string `gorm:"size:255"` // URL to their profile picture
-	Bio            string `gorm:"type:text"`
+	UUID           uuid.UUID `json:"id" gorm:"primaryKey;default:uuid_generate_v4()"`
+	Username       string    `json:"username" gorm:"unique;not null"`
+	FirstName      string    `json:"first_name" gorm:"not null"`
+	LastName       string    `json:"last_name" gorm:"not null"`
+	Email          string    `json:"email" gorm:"unique;not null"`
+	PasswordHash   string    `json:"password_hash" gorm:"not null"`
+	ProfilePicture string    `json:"profile_picture" gorm:"size:255"` // URL to their profile picture
+	Bio            string    `json:"bio" gorm:"type:text"`
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
 	// Cafes          []Cafe    `gorm:"foreignKey:OwnerID"` // User can own multiple cafes
